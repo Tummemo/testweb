a1e5e90185749feb098bad9b59556584cb1b611d
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-28 05:11:38 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
+echo "<center>";
+echo "<form action='index.php' method='post'>";
+echo "<input type='submit' name='submit'>";
+echo "</form>'";
+echo "</center>";
+
+if(isset($_POST["submit"])){
+
+    fopen("https://github.com/Tummemo/testweb/test.txt","a");
+
+}
+
+?>
 
